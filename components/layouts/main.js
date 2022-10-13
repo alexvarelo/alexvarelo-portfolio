3e99fd6d4704cec43a89e949fa9350f6d5732004
avarela <avarela@minsait.com>
2022-10-13 20:37:56 +0200

    solve responsive

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
     return (
         <Box as="main" pb={8}>
             <Head>
-                <meta name="viewport" comment="width-device-width, initial-scale=1" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>Alejandro Varela - Homepage</title>
             </Head>
 
